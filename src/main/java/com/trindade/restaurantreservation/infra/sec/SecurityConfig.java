6ebcb8a83a7553832fc2ed889e3110028e4d7ddd
trindadeeesx <trindadesx@proton.me>
2025-11-22 06:07:49 -0300

    :no_entry_sign: fix: adjust CORS configuration for h2-console

--- src/main/java/com/trindade/restaurantreservation/infra/sec/SecurityConfig.java
+++ src/main/java/com/trindade/restaurantreservation/infra/sec/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.trindade.restaurantreservation.infra.sec;
 
-import com.trindade.restaurantreservation.infra.sec.CustomUserDetailsService;
-import com.trindade.restaurantreservation.infra.sec.SecFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +8,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,7 +26,7 @@ public class SecurityConfig {
 	private CustomUserDetailsService customUserDetailsService;
 
 	@Autowired
-	SecFilter secFilter;
+	private SecFilter secFilter;
 
 	/**
 	 * Configura o filtro de segurança e as regras de autorização.
@@ -37,25 +37,39 @@ public class SecurityConfig {
 	 */
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.cors(cors -> {})
-				.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(authorize -> authorize
-						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-						.anyRequest().authenticated()
-				)
-				.addFilterBefore(secFilter, UsernamePasswordAuthenticationFilter.class);
+			http
+					.csrf(AbstractHttpConfigurer::disable)
+					.cors(cors -> {
+					})
+					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+					.authorizeHttpRequests(auth -> auth
+							// Libera endpoints de autenticação
+							.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+							.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 
-		return http.build();
-	}
+							// Libera H2 Console
+							.requestMatchers("/h2-console/**").permitAll()
 
-	/**
-	 * Configura o codificador de senhas usando BCrypt.
-	 *
-	 * @return O codificador de senhas.
-	 */
+							// Demais exigem token
+							.anyRequest().authenticated()
+					)
+
+					// Necessário para carregar H2 no navegador
+					.headers(headers ->
+							headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+					)
+
+					// Filtro JWT
+					.addFilterBefore(secFilter, UsernamePasswordAuthenticationFilter.class);
+
+			return http.build();
+		}
+
+		/**
+		 * Configura o codificador de senhas usando BCrypt.
+		 *
+		 * @return O codificador de senhas.
+		 */
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
