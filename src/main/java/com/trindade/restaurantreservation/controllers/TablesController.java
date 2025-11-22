23613b8ff63cedc9963bd981e9914e205d7c7948
trindadeeesx <trindadesx@proton.me>
2025-11-22 06:39:48 -0300

    :sparkles: feat: create new REST endpoints

--- src/main/java/com/trindade/restaurantreservation/controllers/TablesController.java
+++ src/main/java/com/trindade/restaurantreservation/controllers/TablesController.java
@@ -1,4 +1,53 @@
 package com.trindade.restaurantreservation.controllers;
 
+import com.trindade.restaurantreservation.dtos.RestaurantTableRequestDTO;
+import com.trindade.restaurantreservation.repos.RestaurantTableRepo;
+import com.trindade.restaurantreservation.services.RestaurantTableService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/restaurant-tables")
+@RequiredArgsConstructor
 public class TablesController {
+	/**
+	 * GET /mesas — Lista todas as mesas e seus status.
+	 * 			POST /mesas — Adiciona uma nova mesa (apenas administradores).
+	 * 	PATCH /mesas/:id — Atualiza informações de uma mesa.
+	 * 			DELETE /mesas/:id — Remove uma mesa (apenas administradores).
+	 */
+
+	private final RestaurantTableService service;
+
+	@GetMapping("/")
+	public ResponseEntity<?> getOne() {
+		return ResponseEntity.status(HttpStatus.OK).body("mesa");
+	}
+
+	@PostMapping("/")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> create(@Valid @RequestBody RestaurantTableRequestDTO dto) {
+		var table = this.service.create(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(table);
+	}
+
+//	@PatchMapping("/{id}")
+//	public ResponseEntity<?> update(
+//			@PathVariable Long id,
+//			@RequestBody UpdateTableDTO updateData
+//	) {
+//		var table = this.service.updateTable(id, updateData);
+//		return ResponseEntity.ok(table);
+//	}
+//
+//	@DeleteMapping("/{id}")
+//	@PreAuthorize("hasRole('ADMIN')")
+//	public ResponseEntity<?> delete(@PathVariable Long id) {
+//		this.service.deleteTable(id);
+//		return ResponseEntity.noContent().build();
+//	}
 }
