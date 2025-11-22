4a150d9c645ec0353d41424d735a4c581ce3abdd
trindadeeesx <trindadesx@proton.me>
2025-11-22 06:09:27 -0300

    :sparkles: feat: implement countByStatus function in repo

--- src/main/java/com/trindade/restaurantreservation/repos/ReservationRepo.java
+++ src/main/java/com/trindade/restaurantreservation/repos/ReservationRepo.java
@@ -1,6 +1,9 @@
 package com.trindade.restaurantreservation.repos;
 
 import com.trindade.restaurantreservation.domain.reservation.Reservation;
+import com.trindade.restaurantreservation.domain.reservation.ReservationState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepo extends JpaRepository<Reservation, String> {}
+public interface ReservationRepo extends JpaRepository<Reservation, String> {
+	long countByStatus(ReservationState status);
+}
--- src/main/java/com/trindade/restaurantreservation/repos/RestaurantTableRepo.java
+++ src/main/java/com/trindade/restaurantreservation/repos/RestaurantTableRepo.java
@@ -1,6 +1,9 @@
 package com.trindade.restaurantreservation.repos;
 
 import com.trindade.restaurantreservation.domain.table.RestaurantTable;
+import com.trindade.restaurantreservation.domain.table.TableState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RestaurantTableRepo extends JpaRepository<RestaurantTable, String> {}
+public interface RestaurantTableRepo extends JpaRepository<RestaurantTable, String> {
+	long countByStatus(TableState status);
+}
