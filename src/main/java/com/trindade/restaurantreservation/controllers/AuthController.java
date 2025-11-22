992b231344c17e296e7d15b3dacc2b5a40a892dc
trindadeeesx <trindadesx@proton.me>
2025-11-22 06:22:05 -0300

    :broom: cleanup: remove unused files and imports

--- src/main/java/com/trindade/restaurantreservation/controllers/AuthController.java
+++ src/main/java/com/trindade/restaurantreservation/controllers/AuthController.java
@@ -1,33 +1,23 @@
 package com.trindade.restaurantreservation.controllers;
 
-import com.trindade.restaurantreservation.domain.user.User;
-import com.trindade.restaurantreservation.domain.user.UserRole;
 import com.trindade.restaurantreservation.dtos.LoginRequestDTO;
 import com.trindade.restaurantreservation.dtos.LoginResponseDTO;
 import com.trindade.restaurantreservation.dtos.RegisterRequestDTO;
 import com.trindade.restaurantreservation.dtos.RegisterResponseDTO;
-import com.trindade.restaurantreservation.infra.sec.Token;
-import com.trindade.restaurantreservation.repos.UserRepo;
 import com.trindade.restaurantreservation.services.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-	private final UserRepo userRepo;
-	private final PasswordEncoder passwordEncoder;
-	private final Token tokenService;
 	private final AuthService service;
 
 	@PostMapping("/register")
